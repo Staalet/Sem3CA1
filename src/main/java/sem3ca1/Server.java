978b78561cffd13b4b4cfbbba9efda183adcd328
coej94 <coej94@hotmail.com>
2017-02-13 14:15:37 +0100

    En spæd start til en server.

--- src/main/java/sem3ca1/Server.java
+++ src/main/java/sem3ca1/Server.java
@@ -5,10 +5,35 @@
  */
 package sem3ca1;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author christian
  */
 public class Server {
     
+    private boolean keepRunning;
+    private static ServerSocket serverSocket;
+    private String myIP;
+    private int myPort;
+    public static void main(String[] args) {
+        
+    }
+    
+    private void RunServer(String ip, int port){
+            myIP = ip;
+            myPort = port;
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(myIP, port));
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }

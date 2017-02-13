ef656f571928373270ed136c1fb7f84d319ae94d
coej94 <coej94@hotmail.com>
2017-02-13 18:21:39 +0100

    Basic Skeleton added to serverclass

--- src/main/java/sem3ca1/Server.java
+++ src/main/java/sem3ca1/Server.java
@@ -8,6 +8,7 @@ package sem3ca1;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,10 +18,11 @@ import java.util.logging.Logger;
  */
 public class Server {
     
-    private boolean keepRunning;
+    private boolean keepRunning = true;
     private static ServerSocket serverSocket;
     private String myIP;
     private int myPort;
+    ArrayList<ClientHandling> clients = new ArrayList<>();
     public static void main(String[] args) {
         
     }
@@ -30,10 +32,20 @@ public class Server {
             myPort = port;
         try {
             serverSocket = new ServerSocket();
-            serverSocket.bind(new InetSocketAddress(myIP, port));
+            serverSocket.bind(new InetSocketAddress(myIP, myPort));
             
+            while(keepRunning){
+                serverSocket.accept();
+                
+            }
+                
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    private void stopServer(){
+        keepRunning = false;
+    }
+   
 }

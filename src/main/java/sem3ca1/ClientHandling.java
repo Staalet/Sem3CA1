6da66ad78538b6ab34cda8271757dd3acaf1a2e8
madsr <madsr@10.50.137.90>
2017-02-14 12:42:40 +0100

    Fjernet overflødige imports

--- src/main/java/sem3ca1/ClientHandling.java
+++ src/main/java/sem3ca1/ClientHandling.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Arrays;
-import java.util.Observable;
-import java.util.Observer;
 import java.util.Scanner;
 
 /**

3db1fc73a54e8603b1a070a7036fd3c95d34317e
Staalet <staalthomas1@gmail.com>
2017-02-17 13:08:35 +0100

    Made some changes and made test for tomcat server with REST

---
+++ src/test/java/sem3ca1/TomcatTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sem3ca1;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author Staal
+ */
+public class TomcatTest {
+
+    public TomcatTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+    
+
+    @Test 
+    public void  tomcatTest() {
+
+        when().
+                get("https://139.59.148.148.xip.io").                 
+                then().
+                statusCode(200);
+                
+
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
